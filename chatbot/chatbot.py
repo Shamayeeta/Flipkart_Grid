d7d3af72cc6217d1ca193fc8ce7615d8c2f307dc
Aadarsh <aadarshshaw05@gmail.com>
2023-08-20 14:45:39 +0530

    remove chatbot add gitignore

--- chatbot/chatbot.py
+++
@@ -1,9 +0,0 @@
-import chainlit as cl
-
-@cl.on_chat_start
-async def start():
-    await cl.Message(content="Hello World!").send()
-    
-@cl.on_message
-async def message(msg):
-    await cl.Message(content="You said: " + msg).send()
